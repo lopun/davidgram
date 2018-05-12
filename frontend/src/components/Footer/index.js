eeb01426793a037d3e1e31d8e573c3c51db43466
ujin43255252 <david16@kaist.ac.kr>
2018-05-12 23:20:25 +0900

    React-configure

---
+++ frontend/src/components/Footer/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Footer = (props, context) => (
+  <footer>
+    <div>
+      <nav>
+        <ul>
+          <li>About us</li>
+          <li>Support</li>
+          <li>Blog</li>
+          <li>Press</li>
+          <li>API</li>
+          <li>Jobs</li>
+          <li>Privacy</li>
+          <li>Terms</li>
+          <li>Directory</li>
+          <li>Language</li>
+        </ul>
+      </nav>
+    </div>
+    <div>
+      <span>(c)nomadgram</span>
+    </div>
+  </footer>
+);
--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -2,6 +2,8 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { routerReducer, routerMiddleware } from "react-router-redux";
 import createHistory from "history/createBrowserHistory";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { i18nState } from "redux-i18n";
 import users from "redux/modules/uesrs";
 
 const env = process.env.NODE_ENV;
@@ -17,11 +19,17 @@ if (env === "development") {
 
 const reducer = combineReducers({
   users,
-  routing: routerReducer
+  routing: routerReducer,
+  i18nState
 });
 
-let store = initialState =>
-  createStore(reducer, applyMiddleware(...middlewares));
+let store;
+if (env === "development") {
+  store = initialState =>
+    createStore(reducer, composeWithDevTools(applyMiddleware(...middlewares)));
+} else {
+  store = initialState => createStore(reducer, applyMiddleware(...middlewares));
+}
 
 export { history };
 
