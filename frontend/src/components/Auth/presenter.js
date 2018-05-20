ef752ae238789ebe258905273c22ed4ec464f584
ujin43255252 <david16@kaist.ac.kr>
2018-05-21 02:04:09 +0900

    Main

--- frontend/src/components/Auth/presenter.js
+++ frontend/src/components/Auth/presenter.js
@@ -8,36 +8,22 @@ const Auth = (props, context) => (
     </div>
     <div className={styles.column}>
       <div className={styles.whiteBox}>
-        {(() => {
-          switch (props.action) {
-            case "login":
-              return (
-                <p>
-                  Don't have an account?{" "}
-                  <span
-                    className={styles.changeLink}
-                    onClick={props.changeAction}
-                  >
-                    Sign up
-                  </span>
-                </p>
-              );
-            case "signup":
-              return (
-                <p>
-                  Have an account?{" "}
-                  <span
-                    className={styles.changeLink}
-                    onClick={props.changeAction}
-                  >
-                    Sign up
-                  </span>
-                </p>
-              );
-            default:
-              return null;
-          }
-        })()}
+        {props.action === "login" && (
+          <p>
+            Don't have an account?{" "}
+            <span className={styles.changeLink} onClick={props.changeAction}>
+              Sign up
+            </span>
+          </p>
+        )}
+        {props.action === "signup" && (
+          <p>
+            Have an account?{" "}
+            <span className={styles.changeLink} onClick={props.changeAction}>
+              Sign up
+            </span>
+          </p>
+        )}
       </div>
       <div className={styles.appBox}>
         <span>Get the app</span>
