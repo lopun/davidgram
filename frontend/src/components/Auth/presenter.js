c3182ac146f249d050511ba5b0502927651be105
ujin43255252 <david16@kaist.ac.kr>
2018-05-21 03:24:58 +0900

    mixin&footer edited

--- frontend/src/components/Auth/presenter.js
+++ frontend/src/components/Auth/presenter.js
@@ -15,7 +15,7 @@ const Auth = (props, context) => (
       </div>
       <div className={styles.whiteBox}>
         {props.action === "login" && (
-          <p>
+          <p className={styles.text}>
             Don't have an account?{" "}
             <span className={styles.changeLink} onClick={props.changeAction}>
               Sign up
@@ -23,7 +23,7 @@ const Auth = (props, context) => (
           </p>
         )}
         {props.action === "signup" && (
-          <p>
+          <p className={styles.text}>
             Have an account?{" "}
             <span className={styles.changeLink} onClick={props.changeAction}>
               Sign up
--- frontend/src/components/AuthForms/index.js
+++ frontend/src/components/AuthForms/index.js
@@ -4,37 +4,51 @@ import Ionicon from "react-ionicons";
 import styles from "./styles.scss";
 
 export const LoginForm = props => (
-  <div>
-    <form>
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <input type="submit" value="Log in" />
+  <div className={styles.formComponent}>
+    <form className={styles.form}>
+      <input className={styles.textInput} type="text" placeholder="Username" />
+      <input
+        className={styles.textInput}
+        type="password"
+        placeholder="Password"
+      />
+      <input className={styles.button} type="submit" value="Log in" />
     </form>
-    <span>or</span>
-    <span>
+    <span className={styles.divider}>or</span>
+    <span className={styles.facebookLink}>
       <Ionicon icon="logo-facebook" fontSize="20px" color="#385185" />
       Log in with Facaebook
     </span>
-    <span>Forgot password?</span>
+    <span className={styles.forgotLink}>Forgot password?</span>
   </div>
 );
 
 export const SignupForm = props => (
-  <div>
-    <h3>Sign up to see photos and videos from your friends.</h3>
-    <button>
+  <div className={styles.formComponent}>
+    <h3 className={styles.signupHeader}>
+      Sign up to see photos and videos from your friends.
+    </h3>
+    <button className={styles.button}>
       <Ionicon icon="logo-facebook" fontSize="20px" color="white" />
       Log in With Facebook
     </button>
-    <span>or</span>
-    <form>
-      <input type="email" placeholder="Email" />
-      <input type="text" placeholder="Full Name" />
-      <input type="username" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <input type="submit" value="Sign up" />
+    <span className={styles.divider}>or</span>
+    <form className={styles.form}>
+      <input className={styles.textInput} type="email" placeholder="Email" />
+      <input className={styles.textInput} type="text" placeholder="Full Name" />
+      <input
+        className={styles.textInput}
+        type="username"
+        placeholder="Username"
+      />
+      <input
+        className={styles.textInput}
+        type="password"
+        placeholder="Password"
+      />
+      <input className={styles.button} type="submit" value="Sign up" />
     </form>
-    <p>
+    <p className={styles.terms}>
       By signing up, you agree to our <span>Terms & Privacy Policy</span>.
     </p>
   </div>
