cbfdf437ec367116d78805dda85a8c1873bed09c
ujin43255252 <ujin43255252@gmail.com>
2018-07-19 02:25:40 +0900

    basepy

--- config/settings/base.py
+++ config/settings/base.py
@@ -38,6 +38,17 @@ USE_TZ = True
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'davidgram',
+        'USER': 'david',
+        'PASSWORD': 'kjh5270!@#@!',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
 DATABASES['default']['ATOMIC_REQUESTS'] = True
 
 # URLS
