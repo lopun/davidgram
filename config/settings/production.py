4af6965f3a38e610d2c01fec483ed810413509d1
ujin43255252 <ujin43255252@gmail.com>
2018-07-20 15:02:43 +0900

    allowed_hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -8,8 +8,8 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['davidgram.ap-northeast-2.elasticbeanstalk.com'])
-
+# ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['davidgram.ap-northeast-2.elasticbeanstalk.com'])
+ALLOWED_HOSTS = ['*']
 # DATABASES
 # ------------------------------------------------------------------------------
 # DATABASES['default'] = env.db('DATABASE_URL')  # noqa F405
