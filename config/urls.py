dd546e67f91f59bdda631dd1e9768d5a9646da9d
ujin43255252 <ujin43255252@gmail.com>
2018-07-20 16:31:47 +0900

    why!!

--- config/urls.py
+++ config/urls.py
@@ -11,7 +11,8 @@ urlpatterns = [
     # Django Admin, use {% url 'admin:index' %}
     url(settings.ADMIN_URL, admin.site.urls),
     # User management
-    url(r'^', include('django.contrib.auth.urls')),
+    # url(r'^', include('django.contrib.auth.urls')),
+    url(r'^admin/', admin.site.urls),
     url(r'^rest-auth/', include('rest_auth.urls')),
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
     url(r"^users/", include("davidgram.users.urls", namespace="users")),
